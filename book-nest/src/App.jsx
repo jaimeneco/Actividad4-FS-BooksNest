af11e06618cfe77286b64772546a598baf6b27ae
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-02-06 13:30:41 +0100

    up

--- book-nest/src/App.jsx
+++ book-nest/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//Import de componentes
+import { Header } from './components/Header';
+import { Header2 } from './components/Header2';
+import { Footer } from './components/Footer';
 
+//Import de Páginas
+import Home from './pages/Home';
+import InicioSesion from './pages/InicioSesion';
+import TyC from './pages/TyC';
+import Perfil from './pages/Perfil';
+import LibroUnidad from './pages/LibroUnidad';
+
+// importar hooks:
+import { Component, useState} from 'react'
+
+
+//-------------------------------------------------
+//--------------INICIO DE LA APP-------------------
+//-------------------------------------------------
 function App() {
-  const [count, setCount] = useState(0)
+  const [nombre, setNombre] = useState("Jaime")
+
+  //Obtener un query param desde la URL "index.html?page=about"
+  const params = new URLSearchParams(window.location.search);
+  const selector = params.get('page') || "home";
+  console.log("query Param es: ", selector)
+
+
+  // //Tambié podría utilizar el Path luego del "/"
+  // const pathValue = window.location.pathname.slice(1) || "home";
+  //   console.log(window.location.pathname, " =>", pathValue);
+
+
+  let page;
+  //Si quisiéramos usar Header o Header 2 hay que cambiar const params o cons pathValue
+  switch(selector){
+    case 'Home': 
+      page = <Home />; 
+      break;
+    case 'Perfil': 
+      page = <Perfil />; 
+      break;
+    case 'LibroUnidad': 
+      page = <LibroUnidad />; 
+      break;
+    case 'TyC': 
+      page = <TyC />; 
+      break;
+    case 'InicioSesion': 
+      page = <InicioSesion />; 
+      break;
+    default: page = <Home />;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Header>Con Query Params</Header>
+      {/* <Header2>Con PathName</Header2> */}
+
+      {page}
+
+      <Footer>Footer</Footer>
+      {/* {  page == "home" ? <Home /> : <Contacto /> } */}
+
+      {/* {  selector == "home" && <Home /> }
+      {  selector == "contacto" && <Home /> }
+      {  selector == "about us" && <Home /> }
+      
+      <Home />
+      <Contacto /> */}
     </>
   )
 }
 
 export default App
+
